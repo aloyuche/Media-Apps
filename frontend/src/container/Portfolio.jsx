b4ff1a439e8ba8de368f9b2c40afecc0fa8a8df3
aloyuche <37053321+aloyuche@users.noreply.github.com>
2022-11-23 16:55:06 +0100

    Update Portfolio.jsx

--- frontend/src/container/Portfolio.jsx
+++ frontend/src/container/Portfolio.jsx
@@ -36,14 +36,14 @@ const Portfolio = () => {
   return (
     <div
       name="portfolio"
-      className="bg-gradient-to-b pt-10 gap-5 h-full from-black to-gray-800 w-full text-white md:h-screen"
+      className="bg-gradient-to-b pt-6 gap-5 h-full from-black to-gray-800 w-full text-white md:h-screen"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-        <div className="pb-8">
-          <p className="text-4xl py-5 font-bold inline border-b-4 border-gray-500">
+        <div className="pb-4">
+          <p className="text-4xl py-3 font-bold inline border-b-4 border-gray-500">
             Portfolio
           </p>
-          <p className="py-6">Check out some of my work right here</p>
+          <p className="py-2">Check out some of my work right here</p>
         </div>
 
         <div className="grid mb-23 sm:grid-cols-2 pb-29 md:grid-cols-3 gap-8 px-12 sm:px-0">
