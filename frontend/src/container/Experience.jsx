4597639adfa2b83ec9acdb5d26c6a0f83aa6bd49
aloyuche <37053321+aloyuche@users.noreply.github.com>
2022-11-23 16:51:52 +0100

    Update Experience.jsx

--- frontend/src/container/Experience.jsx
+++ frontend/src/container/Experience.jsx
@@ -64,16 +64,16 @@ const Experience = () => {
   return (
     <div
       name="experience"
-      className="bg-gradient-to-b pt-16 from-gray-800 to-black w-full h-full"
+      className="bg-gradient-to-b pt-10 from-gray-800 to-black w-full h-full"
     >
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
         <div>
-          <p className="text-4xl mt-12 font-bold border-b-4 border-gray-500 p-2 inline">
+          <p className="text-4xl pt-2 font-bold border-b-4 border-gray-500 p-2 inline">
             Experience
           </p>
           <p className="py-6">These are the Technologies I have worked with</p>
         </div>
-        <div className="w-full mb-32 grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
+        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
           {techs.map(({ id, src, title, style }) => (
             <div
               key={id}
