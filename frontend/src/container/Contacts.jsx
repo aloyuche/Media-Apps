b7db7d76e2b4ee0f306de8636119db38ca5250e3
aloyuche <37053321+aloyuche@users.noreply.github.com>
2022-11-23 16:46:34 +0100

    Update Contacts.jsx

--- frontend/src/container/Contacts.jsx
+++ frontend/src/container/Contacts.jsx
@@ -4,11 +4,11 @@ const Contacts = () => {
   return (
     <div
       name="contact"
-      className="w-full pt-6 h-full bg-gradient-to-b from-black to-gray-800 p-4 text-white"
+      className="w-full pt-2 h-full bg-gradient-to-b from-black to-gray-800 p-4 text-white"
     >
       <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
         <div className="pb-8">
-          <p className="text-4xl mt-6 py-2 font-bold inline border-b-4 border-gray-500">
+          <p className="text-4xl py-2 font-bold inline border-b-4 border-gray-500">
             Contacts
           </p>
           <p className="py-6">Submit the form below to get in touch with me</p>
