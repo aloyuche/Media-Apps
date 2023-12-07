ec44d3325f4faf4dbab474917c9d45f157f62ce5
aloyuche <37053321+aloyuche@users.noreply.github.com>
2023-12-07 17:57:46 +0100

    Update Portfolio.jsx

--- frontend/src/container/Portfolio.jsx
+++ frontend/src/container/Portfolio.jsx
@@ -4,7 +4,7 @@ import installNode from "../asset/p2.jpg";
 import navbar from "../asset/p6.jpg";
 import reactParallax from "../asset/p4.jpg";
 import reactSmooth from "../asset/p1.jpg";
-import reactWeather from "../asset/p9.jpg";
+import reactWeather from "../asset/p0.jpg";
 const Portfolio = () => {
   const portfolios = [
     {
